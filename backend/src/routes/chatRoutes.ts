dea712bb0af33b081cd28c9cc9056b3119137d37
tasnim lafi <tasnimlafi89@gmail.com>
2026-02-03 14:07:01 +0100

    feat: implement chat and message retrieval, add user fetching functionality

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,4 +1,13 @@
 import { Router } from 'express' ;
+import { protectRoute } from '../middleware/auth';
+import { getChats, getOrCreateChat } from '../controllers/chatController';
 const router = Router();
+
+//instead of adding "protecteRoute" to each route like this < router.get("/",protectRoute,getChats); > we can
+
+router.use(protectRoute);
+
+router.get("/",getChats);
+router.post("/with/:participantId",getOrCreateChat);
 
 export default router;
