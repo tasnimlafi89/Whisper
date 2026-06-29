53f9cc90dd158668c01010f3e38e4728fc133337
tasnim lafi <tasnimlafi89@gmail.com>
2026-06-29 16:28:37 +0100

    feat: set up backend server with Express and MongoDB integration
    - Added server initialization in index.ts with Bun
    - Created app.ts to define Express routes and middleware
    - Implemented MongoDB connection logic in database.ts
    - Defined Chat, Message, and User models with Mongoose
    - Established API routes for authentication, chats, messages, and users
    - Updated package.json with necessary dependencies and scripts
    - Configured TypeScript settings in tsconfig.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import {connectDB} from "./src/config/database";
+import app from "./src/app"
+
+const PORT = process.env.PORT || 3000
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running on PORT:",PORT);
+    })
+})
